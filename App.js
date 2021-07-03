0e345fb9f34d732c31fd22784bc8841a825f869f
NS 7u7 <odanaya4@misena.edu.co>
2021-07-02 20:14:59 -0500

    Primera Version

--- App.js
+++ App.js
@@ -1,12 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+import { StyleSheet, Text, View, Dimensions, Platform } from 'react-native';
+
+
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+  let latitudeNumber = "";
+  let longitudeNumber = "";
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+    longitudeNumber = JSON.stringify(location.coords.longitude);
+    latitudeNumber = JSON.stringify(location.coords.latitude);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} 
+      region ={{
+        latitude: Number(latitudeNumber),
+        longitude: Number(longitudeNumber),
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05,
+      }}
+      >
+        <MapView.Marker
+          coordinate = {{
+            latitude: Number(latitudeNumber),
+            longitude: Number(longitudeNumber)
+          }}
+        >
+          <MapView.Callout>
+            <Text>Tú estas aqui</Text>
+          </MapView.Callout>
+        </MapView.Marker>
+
+      </MapView>
     </View>
   );
 }
@@ -17,5 +66,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
